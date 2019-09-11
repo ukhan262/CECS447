e535fdc2845cd9b402613ac91677cccc758637e8
Umar Khan <umarkhan252@gmail.com>
2019-09-11 13:55:11 -0700

    adding first lab

---
+++ Lab1/main.c
@@ -0,0 +1,109 @@
+// main.c
+// Runs on LM4F120 or TM4C123
+// C2_Toggle_PF1, toggles PF1 (red LED) at 5 Hz
+// Daniel Valvano, Jonathan Valvano, and Ramesh Yerraballi
+// January 18, 2016
+
+// LaunchPad built-in hardware
+// SW1 left switch is negative logic PF4 on the Launchpad
+// SW2 right switch is negative logic PF0 on the Launchpad
+// red LED connected to PF1 on the Launchpad
+// blue LED connected to PF2 on the Launchpad
+// green LED connected to PF3 on the Launchpad
+
+#include "SysTick.h"
+
+#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
+#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
+#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
+#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
+#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
+#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
+#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
+#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
+#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
+#define PF4                     (*((volatile unsigned long *)0x40025040))
+#define PF3                     (*((volatile unsigned long *)0x40025020))
+#define PF2                     (*((volatile unsigned long *)0x40025010))
+#define PF1                     (*((volatile unsigned long *)0x40025008))
+#define PF0                     (*((volatile unsigned long *)0x40025004))
+#define GPIO_PORTF_DR2R_R       (*((volatile unsigned long *)0x40025500))
+#define GPIO_PORTF_DR4R_R       (*((volatile unsigned long *)0x40025504))
+#define GPIO_PORTF_DR8R_R       (*((volatile unsigned long *)0x40025508))
+#define GPIO_LOCK_KEY           0x4C4F434B  // Unlocks the GPIO_CR register
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
+
+#define T1ms 16000    // assumes using 16 MHz PIOSC (default setting for clock source)
+#define NVIC_EN0_R              (*((volatile unsigned long *)0xE000E100))  // IRQ 0 to 31 Set Enable Register
+#define NVIC_PRI7_R             (*((volatile unsigned long *)0xE000E41C))  // IRQ 28 to 31 Priority Register
+#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
+#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
+#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
+#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
+#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
+#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
+#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
+#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
+#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
+#define GPIO_PORTF_IS_R         (*((volatile unsigned long *)0x40025404))
+#define GPIO_PORTF_IBE_R        (*((volatile unsigned long *)0x40025408))
+#define GPIO_PORTF_IEV_R        (*((volatile unsigned long *)0x4002540C))
+#define GPIO_PORTF_IM_R         (*((volatile unsigned long *)0x40025410))
+#define GPIO_PORTF_ICR_R        (*((volatile unsigned long *)0x4002541C))
+#define GPIO_PORTF_RIS_R        (*((volatile unsigned long *)0x40025414))
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
+	
+#define RED  0x02;
+#define GREEN 0x08;
+#define BLUE 0x04;
+
+// Subroutine to initialize port F pins for input and output
+void Init_PortF(void){ volatile unsigned long delay;
+  SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
+  delay = SYSCTL_RCGC2_R;           // delay  
+  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0  
+  GPIO_PORTF_CR_R = 0x14;           // allow changes to PF4, PF2 
+  GPIO_PORTF_DEN_R = 0x14;          // 7) enable digital pins PF4, PF2  
+  GPIO_PORTF_AMSEL_R &= ~0x14;        // 3) disable analog function
+  GPIO_PORTF_PCTL_R &=0x00000000;   // 4) GPIO clear bit PCTL  
+  GPIO_PORTF_DIR_R |= 0x04;          // 5) PF2 output 
+	GPIO_PORTF_DIR_R &= ~ 0x10;          // 5) PF4 input
+  GPIO_PORTF_AFSEL_R &= ~0x14;        // 6) no alternate function
+  GPIO_PORTF_PUR_R |= 0x14;          // enable pullup resistors on PF2, PF4  
+  GPIO_PORTF_DEN_R |= 0x14;            // 7) enable digital I/O on PF4-0     
+      
+}
+ 
+
+unsigned long led, in, m;       // LED, mode
+volatile unsigned long SW1;
+extern volatile unsigned long flag;
+
+int main(void){  
+  Init_PortF();            // make PF1,PF2,PF3 output and PF4 input
+	SysTick_Init();          // initialize SysTick timer
+	//SW1 = 1;
+	m = 100;
+  while(1){
+	led = led^0x04;            // toggle blue LED, PF2
+  GPIO_PORTF_DATA_R = led;   // output 
+  SysTick_Wait10ms(m);     // 1s delay
+	if (flag == 1)
+		{
+			m= m+100;
+		}
+	}
+}
+
+	
+
+
+// Color    LED(s) PortF
+// dark     ---    0
+// red      R--    0x02
+// blue     --B    0x04
+// green    -G-    0x08
+// yellow   RG-    0x0A
+// sky blue -GB    0x0C
+// white    RGB    0x0E
+// pink     R-B    0x06
